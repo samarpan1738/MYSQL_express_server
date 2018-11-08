217b57d6c9889119cd1c265a70e5c96737ee85fa
Samarpan Harit <haritsamarpan7@gmail.com>
2018-11-08 20:03:02 +0530

    Random edits

--- mysql_scripts/create_table.js
+++ mysql_scripts/create_table.js
@@ -1,14 +1,16 @@
-const mysql=require('mysql2')
-const connection=mysql.createConnection({
-    host:'localhost',
-    database:'testdb',
-    user:'myuser',
-    password:'mypass',
+const mysql = require('mysql2')
+//Synchronous /Task
+const connection = mysql.createConnection({
+  host: 'localhost',
+  database: 'mytestdb',
+  user: 'myuser',
+  password: 'mypass;',
 
 });
-
+//Async Function
 connection.query(
-    `
+  //Backticks for multi-lining strings
+  `
     CREATE TABLE IF NOT EXISTS persons(
         id INTEGER AUTO_INCREMENT PRIMARY KEY,
         name VARCHAR(50) NOT NULL,
@@ -16,13 +18,13 @@ connection.query(
         city VARCHAR(30)
     )
     `,
-    (err,results)=>
-    {
-        if(err)
-        console.error(err);
-        else
-        console.log("Table Created SUccesfully");
-        connection.close();
-            
-    }
+  //Call-back Function.Runs after query has ended.
+  (err, results) => {
+    if (err)
+      console.error(err);
+    else
+      console.log("Table Created SUccesfully");
+    connection.close();
+
+  }
 );
